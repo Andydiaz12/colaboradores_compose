6926047d2885ad2f0806c997939d15afd10bbc46
Pres-DATA <jandrees12@gmail.com>
2023-09-21 09:31:44 -0600

    update

--- app/src/main/java/com/upaep/upaeppersonal/view/features/directory/DirectoryScreen.kt
+++ app/src/main/java/com/upaep/upaeppersonal/view/features/directory/DirectoryScreen.kt
@@ -1,49 +1,63 @@
 package com.upaep.upaeppersonal.view.features.directory
 
-import android.util.Log
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowRight
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.upaep.upaeppersonal.R
 import com.upaep.upaeppersonal.model.entities.features.directory.DirectoryPerson
 import com.upaep.upaeppersonal.view.base.genericcomponents.BaseView
+import com.upaep.upaeppersonal.view.base.theme.Gray_title
 
 @Preview(showSystemUi = true)
 @Composable
 fun DirectoryScreen() {
     val directoryPeople = getPeople()
     val visibleDescription by remember {
-        mutableStateOf(MutableList(directoryPeople.size) { false })
+        mutableStateOf(mutableStateListOf<Boolean>())
     }
+    var searchValue by remember { mutableStateOf("") }
+
+    repeat(directoryPeople.size) {
+        visibleDescription.add(false)
+    }
+
     BaseView(screenName = "DIRECTORIO") {
         DirectoryContent(
             directoryPeople = directoryPeople,
             onPersonClick = { index ->
-                Log.i("debugClicked_1", index.toString())
-                Log.i("debugClicked_2", visibleDescription[index].toString())
-                visibleDescription[index] = !visibleDescription[index]
-                Log.i("debugClicked_3", visibleDescription[index].toString())
+                visibleDescription[index] = visibleDescription[index].let { !it }
             },
-            visibleDescription = visibleDescription
+            visibleDescription = visibleDescription,
+            value = searchValue,
+            onValueChange = { newValue -> searchValue = newValue }
         )
     }
 }
@@ -52,11 +66,36 @@ fun DirectoryScreen() {
 fun DirectoryContent(
     directoryPeople: List<DirectoryPerson>,
     onPersonClick: (Int) -> Unit,
-    visibleDescription: List<Boolean>
+    visibleDescription: List<Boolean>,
+    value: String,
+    onValueChange: (String) -> Unit
 ) {
     Column() {
-        //INPUT
-        Log.i("debugClicked_4", visibleDescription.toString())
+        BasicTextField(
+            value = value,
+            onValueChange = { onValueChange(it) },
+            decorationBox = { innerTextField ->
+                Row(
+                    modifier = Modifier
+                        .border(
+                            width = 2.dp,
+                            color = Gray_title,
+                            shape = RoundedCornerShape(20.dp)
+                        )
+                        .padding(10.dp)
+                ) {
+                    Box(modifier = Modifier.weight(1f)) {
+                        if (value.isEmpty()) {
+                            Text(text = "Buscar")
+                        }
+                        innerTextField()
+                    }
+                    Image(
+                        painter = painterResource(id = R.drawable.icono_lupa),
+                        contentDescription = ""
+                    )
+                }
+            })
         directoryPeople.forEachIndexed { index, person ->
             SinglePersonDesc(
                 directoryPerson = person,
@@ -94,31 +133,39 @@ fun SinglePersonDesc(
             Icon(imageVector = Icons.Default.ArrowRight, contentDescription = "")
         }
         Divider()
-        if (isVisible) PersonDescription(directoryPerson = directoryPerson)
+        AnimatedVisibility(
+            visible = isVisible,
+            enter = expandVertically(expandFrom = Alignment.Top),
+            exit = shrinkVertically(shrinkTowards = Alignment.Bottom)
+        ) {
+            PersonDescription(directoryPerson = directoryPerson)
+        }
     }
 }
 
 @Composable
 fun PersonDescription(directoryPerson: DirectoryPerson) {
-    if (!directoryPerson.departament.isNullOrBlank()) {
-        DescriptionContainer(
-            image = R.drawable.icono_ubicacion_directorio,
-            text = directoryPerson.departament!!
-        )
+    Column {
+        if (!directoryPerson.departament.isNullOrBlank()) {
+            DescriptionContainer(
+                image = R.drawable.icono_ubicacion_directorio,
+                text = directoryPerson.departament!!
+            )
+        }
+        if (!directoryPerson.phone.isNullOrBlank()) {
+            DescriptionContainer(
+                image = R.drawable.icono_telefono_directorio,
+                text = directoryPerson.phone!!
+            )
+        }
+        if (!directoryPerson.mail.isNullOrBlank()) {
+            DescriptionContainer(
+                image = R.drawable.icono_correo_directorio,
+                text = directoryPerson.mail!!
+            )
+        }
+        Divider()
     }
-    if (!directoryPerson.phone.isNullOrBlank()) {
-        DescriptionContainer(
-            image = R.drawable.icono_telefono_directorio,
-            text = directoryPerson.phone!!
-        )
-    }
-    if (!directoryPerson.mail.isNullOrBlank()) {
-        DescriptionContainer(
-            image = R.drawable.icono_correo_directorio,
-            text = directoryPerson.mail!!
-        )
-    }
-    Divider()
 }
 
 @Composable
